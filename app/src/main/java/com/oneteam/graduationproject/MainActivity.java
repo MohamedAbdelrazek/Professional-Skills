d727b1b0fbdd6c10ed7f8a63489e988a7a022489
Mohamed AbdelraZek <muhamedrabi3@gmail.com>
2017-02-17 18:54:49 +0200

    implementing TODO

--- app/src/main/java/com/oneteam/graduationproject/MainActivity.java
+++ app/src/main/java/com/oneteam/graduationproject/MainActivity.java
@@ -1,19 +1,19 @@
 package com.oneteam.graduationproject;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+   private UserSession zUserSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        zUserSession = new UserSession(this);
+        // checking if the user already has email and password
+        //if there is no data this func will launch the login activity && if not it does nothing 
+        zUserSession.checkLogin();
 
-        // TODO: Before starting the LoginActivity check if the user is authorized or not
-
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
     }
 }
