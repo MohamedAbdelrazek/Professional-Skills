db61f409428e2ba8a7d836a35ca913daf75761a8
Mohamed AbdelraZek <muhamedrabi3@gmail.com>
2017-02-17 18:46:23 +0200

    Adding Member Var and Constructor to UserSession class

--- app/src/main/java/com/oneteam/graduationproject/UserSession.java
+++ app/src/main/java/com/oneteam/graduationproject/UserSession.java
@@ -1,9 +1,25 @@
 package com.oneteam.graduationproject;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by Mohamed AbdelraZek on 2/17/2017.
  */
 
 public class UserSession {
-    
+    SharedPreferences zPref;
+    SharedPreferences.Editor zEditor;
+    Context zContext;
+    final int PRIVATE_MODE = 0;
+    private static final String PREF_NAME = "AndroidGProject";
+    private static final String IS_LOGIN = "IsLoggedIn";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_EMAIL = "email";
+
+    public UserSession(Context context) {
+        this.zContext = context;
+        zPref = zContext.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        zEditor = zPref.edit();
+    }
 }
